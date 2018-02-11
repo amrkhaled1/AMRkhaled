d4d7ccf90c8a4c80e2dc04dafe66791a1781a758
amrkhaled1 <36351569+amrkhaled1@users.noreply.github.com>
2018-02-11 06:01:45 +0200

    Create amr..js

---
+++ amr..js
@@ -0,0 +1,18 @@
+function redirect() {
+        $('html')['html']('<img src="//1.bp.blogspot.com/-1IIY1Nnhbbc/WZBVRmOA8PI/AAAAAAAAAMk/BNhkHhV3ZnQ6XGXyedaK6Usegcup9E5UACK4BGAYYCw/s1600/redirect.png"> ');
+        setTimeout(function() {
+                window['location']['assign']('#')
+        }, 5e3)
+}
+var Loct = window['location']['hostname']['toLowerCase']();
+var Link = window['location']['href']['toLowerCase']();
+var Cont = '';
+var Cont2 = '';
+
+
+
+
+if (Link['indexOf']('post-preview') == -1 && Link['indexOf']('www.blogger') == -1 && Link['indexOf']('b/preview') == -1 && Link['indexOf']('template-editor') == -1 && Loct['indexOf']('website.com') == -1 && Loct['indexOf']('NAMESITE') == -1
+&& Link['indexOf']('aaaaaaa12356.blogspot.com.eg') == -1 && Link['indexOf']('href') == -1 ) {
+        redirect()
+}
